7e930e3e816ded8a3fc27563d2a110e41c03582f
SeungW00 <hsw0205@gmail.com>
2017-06-02 15:57:51 +0900

    Add User

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -25,4 +25,21 @@ public class UserDao {
         return user;
 
     }
+
+    public String add(User user) throws SQLException,ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/spring","root","");
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id,name,password) VALUES (?,?,?)");
+        preparedStatement.setString(1,user.getId());
+        preparedStatement.setString(2,user.getName());
+        preparedStatement.setString(3,user.getPassword());
+        preparedStatement.executeUpdate();
+
+        String id = user.getId();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+
+    }
 }
--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -7,18 +7,38 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Administrator on 2017-05-30.
  */
-public class UserDaoTest {
-    @Test
-    public void get() throws SQLException, ClassNotFoundException{
-        UserDao userDao =new UserDao();
-        String id = "hsw";
-        String name = "SeungWoo";
-        String password = "1234";
+        public class UserDaoTest {
+            @Test
+            public void get() throws SQLException, ClassNotFoundException{
+                UserDao userDao =new UserDao();
+                String id = "hsw";
+                String name = "SeungWoo";
+                String password = "1234";
 
         User user =userDao.get(id);
         assertEquals(id,user.getId());
         assertEquals(name,user.getName());
         assertEquals(password,user.getPassword());
+
+    }
+    @Test
+    public void add() throws SQLException, ClassNotFoundException{
+            UserDao userDao = new UserDao();
+        String id = "hsw1111";
+        String name = "SeungWoo1";
+        String password = "1234";
+
+            User user = new User();
+            user.setId(id);
+            user.setName(name);
+            user.setPassword(password);
+
+        userDao.add(user);
+        User resultUser = userDao.get(id);
+        assertEquals(id,resultUser.getId());
+        assertEquals(name,resultUser.getName());
+        assertEquals(password,resultUser.getPassword());
+
 
     }
 }
