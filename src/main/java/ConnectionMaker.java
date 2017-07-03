47732b41bfaceab25e46e2453cdbc2d0f58c8dc6
SeungW00 <hsw0205@gmail.com>
2017-07-03 13:02:49 +0900

    1

--- src/main/java/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-
-/**
- * Created by Administrator on 2017-06-05.
- */
-public interface  ConnectionMaker {
-    Connection getConnection() throws ClassNotFoundException, SQLException;
-}
